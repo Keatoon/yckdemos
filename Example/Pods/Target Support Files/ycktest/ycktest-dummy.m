474d7bced61c56dfb227e636e36269a5186f53ca
godlike <1793558729@qq.com>
2020-04-10 10:43:12 +0800

    基础测试

---
+++ Example/Pods/Target Support Files/ycktest/ycktest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ycktest : NSObject
+@end
+@implementation PodsDummy_ycktest
+@end
---
+++ ycktest/Classes/AViewController.h
@@ -0,0 +1,16 @@
+//
+//  AViewController.h
+//  Pods-ycktest_Example
+//
+//  Created by fangqian on 10/4/2020.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
